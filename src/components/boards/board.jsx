733def12fa2e979495ff394969f4cd5248b50c44
shecho <shechovala@gmail.com>
2020-12-13 23:20:05 -0500

    added some incons on boards

--- src/components/boards/board.jsx
+++ src/components/boards/board.jsx
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
-
-
+import React from "react";
 
 const Board = (props) => {
   const { id, description, name, members } = props.board;
@@ -8,14 +6,28 @@ const Board = (props) => {
     <div className="container text-center" key={id}>
       <div className="col-md-4 m-2 text-center">
         <div className="card bg-dark text-light">
+          <div className="d-flex justify-content-end p-2 ">
           <i className="fa fa-ellipsis-h " aria-hidden="true" />
+
+          </div>
+          <div className="d-flex justify-content-end p-2 ">
+            <i className="fa fa-trash text-danger" aria-hidden="true" />
+          </div>
           <div className="card-body">
             <h5 className="card-title"> {name}</h5>
             <p> {description}</p>
-            <div className="d-flex justify-content-center">
+            <div className="d-flex justify-content-center ">
+              <button type="button" className="btn btn-primary mr-1">
+                <i className="fa fa-check bnt btn-success" aria-hidden="true" />
+                Participantes{" "}
+                <span className="badge bg-secondary p-1">
+                  {" "}
+                  {members.length}
+                </span>
+              </button>
               <button type="button" className="btn btn-primary">
-              <i className="fa fa-check bnt btn-success" aria-hidden="true" />
-                Participantes <span className="badge bg-secondary p-1"> {members.length}</span>
+                <i className="fa fa-plus bnt btn-success" aria-hidden="true" />
+                Invitados <span className="badge bg-secondary p-1"> </span>
               </button>
             </div>
           </div>
--- src/components/boards/boardDetail.jsx
+++ src/components/boards/boardDetail.jsx
@@ -1,6 +1,24 @@
 import React, {useState} from "react"
 import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import arrayMove from 'array-move';
+
+
+const  SortableItem  = SortableElement(({ value }) => <p>{value}</p>);
+
+const SortableList = SortableContainer(({ items }) => {
+    return (
+
+        <div className="card bg-dark text-light m-5 p-5">
+            <ul>
+                {items.map((value, index) => (
+                    <SortableItem key={`item-${value}`} index={index} value={value} />
+                ))}
+            </ul>
+        </div>
+    );
+});
+
+
 const  BoardDetail= ()=>{
   return(
     <div>
