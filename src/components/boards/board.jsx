de67d99250bf1f5f52f3d3453605d28da8d72cf1
shecho <shechovala@gmail.com>
2020-12-13 23:22:39 -0500

    some indentation

--- src/components/boards/board.jsx
+++ src/components/boards/board.jsx
@@ -7,8 +7,7 @@ const Board = (props) => {
       <div className="col-md-4 m-2 text-center">
         <div className="card bg-dark text-light">
           <div className="d-flex justify-content-end p-2 ">
-          <i className="fa fa-ellipsis-h " aria-hidden="true" />
-
+            <i className="fa fa-ellipsis-h " aria-hidden="true" />
           </div>
           <div className="d-flex justify-content-end p-2 ">
             <i className="fa fa-trash text-danger" aria-hidden="true" />
@@ -19,11 +18,8 @@ const Board = (props) => {
             <div className="d-flex justify-content-center ">
               <button type="button" className="btn btn-primary mr-1">
                 <i className="fa fa-check bnt btn-success" aria-hidden="true" />
-                Participantes{" "}
-                <span className="badge bg-secondary p-1">
-                  {" "}
-                  {members.length}
-                </span>
+                Participantes
+                <span className="badge bg-secondary p-1">{members.length}</span>
               </button>
               <button type="button" className="btn btn-primary">
                 <i className="fa fa-plus bnt btn-success" aria-hidden="true" />
