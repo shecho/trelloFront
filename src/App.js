0d5bfab56a224327bf89d0663d67bb6f96f9102d
shecho <shechovala@gmail.com>
2020-12-16 18:48:34 -0500

    added list coponent and cosuming endpint listm

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
   Redirect,
 } from "react-router-dom";
 import BoardDetail from "./components/boards/boardDetail";
+import SortableComponent from "./components/sortableContainer/sortableComponent";
 function App() {
   const [loginStatus, setLoginStatus] = useState(true);
   const handleLogStatus = (status) => {
@@ -41,7 +42,7 @@ function App() {
             <Route exact path="/user" component={User} />
             <Route exact path="/boards" component={Boards} />
             <Route path="/boards/:id" component={BoardDetail} />
-            <Route exact path="/test" component={SortableContainer} />
+            <Route exact path="/test" component={SortableComponent} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/" component={Home} />
           </Switch>
--- src/components/lists/list.jsx
+++ src/components/lists/list.jsx
@@ -5,6 +5,7 @@ const List = (props) => {
   return (
     <div className="text-center" key={id}>
       <div className="card bg-dark text-light">
+          <h5 className="card-title "> <span className='dx-2'> N°{position}</span> {name}</h5>
         <div className="d-flex justify-content-center p-2 ">
           <div className="m-2">
             <i className="fa fa-plus-circle text-success" aria-hidden="true" />
@@ -17,18 +18,32 @@ const List = (props) => {
           </div>
         </div>
         <div className="card-body">
-          <h5 className="card-title"> {name}</h5>
-          <p> {position}</p>
-          <div className="d-flex justify-content-center ">
-            <button type="button" className="btn btn-primary mr-1">
-              <i className="fa fa-check bnt btn-success" aria-hidden="true" />
-              Participantes
-              <span className="badge bg-secondary p-1">{members.length}</span>
-            </button>
-            <button type="button" className="btn btn-primary">
-              <i className="fa fa-plus bnt btn-success" aria-hidden="true" />
-              Invitados <span className="badge bg-secondary p-1"> </span>
-            </button>
+          
+          <div className="justify-content-center ">
+            <div>
+            card
+            </div>
+            <div>
+            card
+            </div>
+            <div>
+            card
+            </div>
+            <div>
+            card
+            </div>
+            <div>
+            card
+            </div>
+            <div>
+            card
+            </div>
+            <div>
+            card
+            </div>
+            <div>
+            card
+            </div>
           </div>
         </div>
       </div>
