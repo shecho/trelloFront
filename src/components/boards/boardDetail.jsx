c591a1aa6c55b4eb6c54f82c5cab186d2f9f365a
shecho <shechovala@gmail.com>
2020-12-16 21:24:44 -0500

    some changes

--- src/components/boards/boardDetail.jsx
+++ src/components/boards/boardDetail.jsx
@@ -1,7 +1,6 @@
-import React, {useState, useffect} from "react"
+import React, {useState, useEffect} from "react"
 import './boards.css'
 // import arrayMove from 'array-move';
-import React, { useState, useEffect } from "react";
 import List from '../lists/list'
 
 const BoardDetail = () => {
