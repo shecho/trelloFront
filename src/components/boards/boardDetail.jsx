57cead4dccfb3596b3bd225ae6bf1217b9236dcb
shecho <shechovala@gmail.com>
2020-12-17 19:45:06 -0500

    merge clahnges and integrations

--- src/components/boards/boardDetail.jsx
+++ src/components/boards/boardDetail.jsx
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from "react";
 import "./boards.css";
-import {useParams} from 'react-router-dom'
+import { useParams } from "react-router-dom";
 
 // import arrayMove from 'array-move';
 import List from "../lists/list";
 
 const BoardDetail = () => {
-   const [list, setList] = useState([]);
-   // const [cards, setCards] = useState([]);
-   useEffect(() => {
-      getList();
-   }, []);
+  const [list, setList] = useState([]);
+  // const [cards, setCards] = useState([]);
+  useEffect(() => {
+    getList();
+  }, []);
 
-
-  const {id} = useParams()
+  const { id } = useParams();
   const getList = async () => {
     let url = `http://127.0.0.1:8000/boards/${id}`;
     let response = await fetch(url);
@@ -22,9 +21,13 @@ const BoardDetail = () => {
   };
   return (
     <div className="list">
-      {list.map((list) => {
-        return <List list={list} key={list.id} />;
-      })}
+      <div className="child_list">
+        <div className="box_list">
+          {list.map((list) => {
+            return <List list={list} key={list.id} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
